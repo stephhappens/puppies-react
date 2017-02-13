4fc8f102666159ba03aaf9da1c44267a398b43ff
Stephanie Winn <stephanie.n.winn@gmail.com>
2017-02-13 17:31:36 -0600

    add sass

--- src/App.js
+++ src/App.js
@@ -1,29 +1,28 @@
-import React, { Component } from 'react';
-import './App.scss';
-import 'bulma';
-
+import React, {Component} from 'react';
+import 'bulma/css/bulma.css';
+import './index.scss';
 
 class App extends Component {
-  render() {
-    return (
-      <nav className="nav">
-        <div className="nav-left">
-          <a href="#" className="nav-item is-brand">
-            Nashville Puppy Adoption
-          </a>
-        </div>
+    render() {
+        return (
+              <nav className="nav">
+                  <div className="nav-left">
+                      <a href="#" className="nav-item is-brand">
+                          Nashville Puppy Adoption
+                      </a>
+                  </div>
 
-        <div className="nav-center">
-          <a href="#" className="nav-item">
-            All Puppies
-          </a>
-          <a href="#" className="nav-item">
-            Add Puppy
-          </a>
-        </div>
-      </nav>
-    );
-  }
+                  <div className="nav-center">
+                      <a href="#" className="nav-item">
+                          All Puppies
+                      </a>
+                      <a href="#" className="nav-item">
+                          Add Puppy
+                      </a>
+                  </div>
+              </nav>
+        );
+    }
 }
 
 export default App;
